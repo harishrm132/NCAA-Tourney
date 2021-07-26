f26a0be34c0d64677467ae01611f47ec1db432cb
Harish <harishrm132@gmail.com>
2021-07-26 23:11:46 +0530

    📦 NEW: Dashboard Window

--- NCAA UI/ViewModels/ShellViewModel.cs
+++ NCAA UI/ViewModels/ShellViewModel.cs
@@ -4,11 +4,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackerLibrary;
+using TrackerLibrary.Models;
 
 namespace NCAA_UI.ViewModels
 {
     public class ShellViewModel : Conductor<object>
     {
+        public ShellViewModel()
+        {
+            //Initillize the database connection
+            GlobalConfig.InitializeConnection(DatabaseType.SQL);
+
+            _existingTournaments = new BindableCollection<TournamentModel>(GlobalConfig.Connection.GetTournaments_All());
+        }
+
+        public void CreateTournament()
+        {
+        }
+
+        private BindableCollection<TournamentModel> _existingTournaments;
+
+        public BindableCollection<TournamentModel> ExistingTournaments
+        {
+            get { return _existingTournaments; }
+            set { _existingTournaments = value; }
+        }
+
+        private TournamentModel _selectedTournament;
+
+        public TournamentModel SelectedTournament
+        {
+            get { return _selectedTournament; }
+            set { 
+                _selectedTournament = value;
+                NotifyOfPropertyChange(() => SelectedTournament);
+            }
+        }
+
 
     }
 }
