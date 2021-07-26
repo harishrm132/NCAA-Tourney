2b1a65afaa500d219e45c8e62905b4ed0e5ec747
Harish <harishrm132@gmail.com>
2021-07-27 00:05:45 +0530

    📦 NEW:  Prize Window added

---
+++ NCAA UI/ViewModels/CreatePrizeFormViewModel.cs
@@ -0,0 +1,93 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackerLibrary;
+using TrackerLibrary.Models;
+
+namespace NCAA_UI.ViewModels
+{
+    public class CreatePrizeFormViewModel : Screen
+    {
+        private int _placeNumber;
+        private string _placeName;
+        private decimal _prizeAmount;
+        private double _prizePercentage;
+
+        public int PlaceNumber
+        {
+            get { return _placeNumber; }
+            set 
+            { 
+                _placeNumber = value;
+                NotifyOfPropertyChange(() => PlaceNumber);
+            }
+        }
+
+        public string PlaceName
+        {
+            get { return _placeName; }
+            set 
+            { 
+                _placeName = value;
+                NotifyOfPropertyChange(() => PlaceName);
+            }
+        }
+
+        public decimal PrizeAmount
+        {
+            get { return _prizeAmount; }
+            set 
+            { 
+                _prizeAmount = value;
+                NotifyOfPropertyChange(() => PrizeAmount);
+            }
+        }
+
+        public double PrizePercentage
+        {
+            get { return _prizePercentage; }
+            set 
+            { 
+                _prizePercentage = value;
+                NotifyOfPropertyChange(() => PrizePercentage);
+            }
+        }
+
+        public bool CanCreatePrize(int placeNumber, string placeName, decimal prizeAmount, double prizePercentage)
+        {
+            return ValidateForm(placeNumber, placeName, prizeAmount, prizePercentage);
+        }
+        
+        public void CreatePrize(int placeNumber, string placeName, decimal prizeAmount, double prizePercentage)
+        {
+            PriceModel model = new PriceModel
+            {
+                PlaceNumber = placeNumber,
+                PlaceName = placeName,
+                PriceAmount = prizeAmount,
+                PricePercentage = prizePercentage
+            };
+
+            //GlobalConfig.Connection.CreatePrize(model);
+
+            //TODO - Close out forma and alert the calling form
+        }
+
+        private bool ValidateForm(int placeNumber, string placeName, decimal prizeAmount, double prizePercentage)
+        {
+            bool output = true;
+
+            if (placeNumber < 1) { output = false; }
+
+            if (PlaceName.Length == 0) { output = false; }
+ 
+            if (prizeAmount <= 0 && (prizePercentage <= 0 || prizePercentage > 100)) { output = false; }
+
+            return output;
+        }
+
+    }
+}
